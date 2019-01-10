4c255359ab8cd50467c0623dac68ee8e6a450a6d
jmketri@gmail.com <jmketri@gmail.com>
2019-01-10 16:47:30 +0900

    change ip and topic name

--- pykakfa_producer.py
+++ pykakfa_producer.py
@@ -3,19 +3,18 @@ from kafka import KafkaConsumer, KafkaProducer
 #import logging
 #logging.basicConfig(level=logging.DEBUG)
 
-producer = KafkaProducer(bootstrap_servers='c7-01:9092')
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
 
 i = 0
 #fd = open('/root/spark-2.1.0-bin-hadoop2.7/tdata/attack.log', 'r')
 #fd = open('/var/log/syslog', 'r')
-fd = open('/home/jaemkim/local/spark/jaemkim/access.log', 'r')
+fd = open('./access.log', 'r')
 while True:
     line = fd.readline()
     i = i+1
     if i > 10:
         break
-    producer.send('test', str(line))
+    producer.send('jmkim', str(line).encode('utf-8'))
     producer.flush()
 
 fd.close()
-
